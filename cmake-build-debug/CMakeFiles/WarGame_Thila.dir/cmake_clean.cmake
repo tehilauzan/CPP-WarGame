dca22b520eeae3e842fda2f87f0f35cc5a9bbefa
tehilauzan <46137999+tehilauzan@users.noreply.github.com>
2020-07-23 10:24:56 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/WarGame_Thila.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WarGame_Thila.dir/main.cpp.o"
+  "WarGame_Thila"
+  "WarGame_Thila.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WarGame_Thila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
